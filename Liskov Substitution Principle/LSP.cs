098aa5d40408e65a5758ccb1b2ad7af60eb9ce94
hjmnz-dev <hamsjimenez@gmail.com>
2024-07-22 23:05:10 -0400

    Add project files.

---
+++ Liskov Substitution Principle/LSP.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaPrincipios.Liskov_Substitution_Principle
+{
+    internal class LSP
+    {
+    }
+}
