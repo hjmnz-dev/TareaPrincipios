d8acdbfcaffc7c448f54d595952cd1412f9024b8
hjmnz-dev <hamsjimenez@gmail.com>
2024-07-30 12:38:59 -0400

    Cargando tarea OCP #2023-0184

---
+++ Open Closed Principle/Course con OCP.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaPrincipios.Open_Closed_Principle
+{
+    public abstract class Course_con_OCP
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+    }
+
+    public class OnlineCourseOCP : Course_con_OCP
+    {
+        public override void Subscribe(Student std)
+        {
+            // Lógica para suscribirse a un curso en línea
+            
+        }
+    }
+
+    public class OfflineCourseOCP : Course_con_OCP
+    {
+        public override void Subscribe(Student std)
+        {
+            // Lógica para suscribirse a un curso fuera de línea
+        }
+    }
+
+    public class HybridCourse : Course_con_OCP
+    {
+        private readonly OnlineCourseOCP onlineCourse;
+        private readonly OfflineCourseOCP offlineCourse;
+
+        public HybridCourse(OnlineCourseOCP onlineCourse, OfflineCourseOCP offlineCourse)
+        {
+            this.onlineCourse = onlineCourse;
+            this.offlineCourse = offlineCourse;
+        }
+
+        public override void Subscribe(Student std)
+        {
+            // Lógica para suscribirse a ambos cursos
+            onlineCourse.Subscribe(std);
+            offlineCourse.Subscribe(std);
+        }
+    }
+
+}
---
+++ Open Closed Principle/Course sin OCP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaPrincipios.Open_Closed_Principle
+{
+    public abstract class Course_sin_OCP
+    {
+        public int CourseId { get; set; }
+        public int Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+
+    }
+
+    public class Student
+    {
+        public string nombre { get; set; }
+    }
+
+    public class OnlineCourse: Course_sin_OCP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+    public class OfflineCourse : Course_sin_OCP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
