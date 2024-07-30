5c8abdaf5a132bc4e3991b5fff294ebcafb6e472
hjmnz-dev <hamsjimenez@gmail.com>
2024-07-30 13:20:16 -0400

    Cargando tarea LSP #2023-0184

---
+++ Liskov Substitution Principle/Course con LSP.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaPrincipios.Open_Closed_Principle;
+
+namespace TareaPrincipios.Liskov_Substitution_Principle
+{
+    public abstract class Course_con_LSP
+    {
+        public int CourseId { get; set; }
+        public string Title{ get; set; }
+        public abstract void Subscribe(Student std);
+        public abstract string GetCourseDetails();
+
+    }
+
+    public class OnlineCourse : Course_con_LSP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+        public override string GetCourseDetails()
+        {
+            return $"Online Course: {Title}, CourseId: {CourseId}";
+        }
+    }
+
+    public class OfflineCourse : Course_con_LSP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+        public override string GetCourseDetails()
+        {
+            return $"Offline Course: {Title}, CourseId: {CourseId}";
+        }
+    }
+
+    public class HybridCourse : Course_con_LSP
+    {
+        private readonly OnlineCourse onlineCourse;
+        private readonly OfflineCourse offlineCourse;
+
+        public HybridCourse(OnlineCourse onlineCourse, OfflineCourse offlineCourse)
+        {
+            this.onlineCourse = onlineCourse;
+            this.offlineCourse = offlineCourse;
+        }
+
+        public override void Subscribe(Student std)
+        {
+            // Lógica para suscribirse a ambos cursos
+            onlineCourse.Subscribe(std);
+            offlineCourse.Subscribe(std);
+        }
+        public override string GetCourseDetails()
+        {
+            return $"Hybrid Course: {Title}, CourseId: {CourseId}\n" +
+                   $"{onlineCourse.GetCourseDetails()}\n" +
+                   $"{offlineCourse.GetCourseDetails()}";
+        }
+    }
+
+}
---
+++ Liskov Substitution Principle/Course sin LSP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TareaPrincipios.Open_Closed_Principle;
+
+namespace TareaPrincipios.Liskov_Substitution_Principle
+{
+    public abstract class Course_sin_LSP
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+    }
+
+    public class OnlineCourseLSP : Course_sin_LSP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class OfflineCourseLSP : Course_sin_LSP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class HybridCourseLSP : Course_sin_LSP
+    {
+        public override void Subscribe(Student std)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
